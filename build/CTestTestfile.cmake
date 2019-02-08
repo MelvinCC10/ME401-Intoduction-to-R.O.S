bf09d07b76cf1cb63f54916730b1e53407de4110
Reven10 <melvin.cordon21@gmail.com>
2019-02-07 17:24:44 -0700

    First Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/reven/melvin_ws/src
+# Build directory: /home/reven/melvin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
