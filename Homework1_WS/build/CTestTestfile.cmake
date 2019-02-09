c4a78f33dba67005629dca7290ed8852fad092f9
Reven10 <melvin.cordon21@gmail.com>
2019-02-09 14:33:42 -0700

    Moved Homework1_WS under a main folder. This was done so other homework workspaces can be added to this repository.

---
+++ Homework1_WS/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/reven/melvin_ws/Homework1_WS/src
+# Build directory: /home/reven/melvin_ws/Homework1_WS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
---
+++ Homework1_WS/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/reven/melvin_ws/Homework1_WS/src/beginner_tutorials/msg")
+set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
