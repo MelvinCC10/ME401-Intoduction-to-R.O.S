2d45dc7525a39fe1022ea1e2eaf6624b32e0b908
Reven10 <melvin.cordon21@gmail.com>
2019-03-05 15:38:11 -0700

    problem 1-3 done

--- homework_ws/src/beginner_tutorials/scripts/cFun.py
+++ homework_ws/src/beginner_tutorials/scripts/cFun.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+import math
 
-# This function will generate a dubins curve passing the through a given set of waypoints
+# This function will generate a dubins curve that passes through a given set of
+# waypoints and return a list of points for the dubin curve.
 # a waypoint =  [x, y, heading in degrees]
-def dubinsGen(wayPoints, turning_radius, step_size)
+def dubinsGen(wayPoints, turning_radius, step_size):
     wpm = []
     for i in range(len(WayPoints)):
         q0 = wayPoints[i]
@@ -15,3 +17,83 @@ def dubinsGen(wayPoints, turning_radius, step_size)
         for i in range(len(configurations)):
             wpm.append(configurations[i])
     return wpm
+
+def checkDis(point1,point2):
+    dis = abs(math.sqrt((point1[0]-point2[0])**2 + (point1[1]-point2[1])**2))
+    return dis
+
+def checkLoc(obstacles, gridBond, currentLoc):
+    st = "Valid"
+    if currentLoc[0] < 0 or currentLoc[0] > gridBond[0] or currentLoc[1] < 0 or currentLoc[1] > gridBond[1]:
+        st =  "Invalid"
+
+    for i in range(len(obstacles)):
+        if obstacles[i] == currentLoc:
+            st = "Invalid"
+            break
+    print st
+
+def nodeIndex(node, bounds):
+    #                   y                  x
+    index = bounds*node[1] - (bounds-node[0])
+    return index
+
+def nodes(bounds):
+    for j in range(bounds):
+        for k in range(bounds):
+            dic[nodeIndex((k+1,j+1),bounds)] = [k+1,j+1,0]
+    return dic
+
+
+
+class NNode:
+    x = 0
+    y = 0
+    Index = 0
+    cost = 0
+    parentID = 0
+
+def createNodes(bounds):
+    NodeKeeper = []
+    for i in range(bounds):
+        for j in range(bounds):
+            x = j+1
+            y = i+1
+            index = nodeIndex([x,y],bounds)
+            N = node
+            NodeKeeper.append()
+        return NodeKeeper
+
+#the starting location, goal location, grid information, and obstacle list.
+def Dijkstra(Start,goal,bounds,obst):
+    keeper = {}
+    keeper = nodes(bounds)
+    startIndex = nodeIndex(Start,bounds)
+    keeper[startIndex].append(0)
+    print keeper[startIndex]
+
+
+
+
+
+
+
+test = True
+if test == True:
+
+    X = checkDis([2,1],[3,2])
+    print (X)
+
+    obs = [(1,1), (4,4), (3,4), (5,0), (5,1), (0,7), (1,7), (2,7), (3,7)]
+    currLoc = (5,1)
+    grid = [2,2]
+    print len(obs)
+
+    checkLoc(obs, grid, currLoc)
+
+    nodeIndex([3,3],10)
+
+    dic = {}
+    nodes(3)
+
+    Dijkstra((3,3),(4,4),10,11)
